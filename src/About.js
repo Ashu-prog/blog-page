b5e1a0e0a15fc9491d825bde3207eb2ce6aad1ef
Ashu-prog <126682426+Ashu-prog@users.noreply.github.com>
2023-03-01 18:47:37 +0530

    Add files via upload

---
+++ src/About.js
@@ -0,0 +1,12 @@
+const About = () => {
+    return (
+        <main className='About'>
+            <h2>About</h2>
+            <p style={{ marginTop: "1rem" }}>This blog app is a project where you can add your daily blogs and search the blog at any time you need.</p>
+            <p style={{ marginTop: "10rem" }}>This app is created by Ramesh Chandra Mohapatra.</p>
+            <p style={{ marginTop: "0.5rem" }}>Software Development Trainee - SearchingYard Software Group.</p>
+        </main>
+    )
+}
+
+export default About
